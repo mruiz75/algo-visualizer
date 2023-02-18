137712f8b260021baaec5a508c02507155597ea0
Manuel Ruiz <manuelra75@gmail.com>
2023-02-18 16:08:29 -0600

    Adding SPA capabilities

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import SortingVisualizer from "./visualizers/sorting";
 import "./App.css";
 import Visualizer from "./visualizers/Visualizer";
+import MainMenu from "./mainMenu";
 
 function App() {
   return (
     <div className="App">
-      <Visualizer />
+      <MainMenu />
     </div>
   );
 }
